2534cca12f3a78a2d0d031343494d244fec891da
Jesus Figueredo <jafigueredo@gmail.com>
2019-07-25 11:00:34 -0300

    implement heatmap.js & leaftlet

--- src/components/HereMap.vue
+++ src/components/HereMap.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div id="heatmap-maplima" />
+    <div id="heatmap-maplima" v-once />
     <div class="preload" style="display: none">
       <div class="imgpreload">
         <img src="static/preload.gif" alt="preload" />
@@ -13,7 +13,11 @@
 </template>
 
 <script>
-const API_KEY = '9S-Qq8kiAq4g1KRquBAyDFAH64crVZ59IpW5MrGcy9c'
+import L from 'leaflet'
+import HeatmapOverlay from '@/scripts/leaflet-heatmap'
+
+import records from '@/data/records'
+// --
 const lines = records.split('\n')
 const points = lines.map(record => record.split(','))
 const headers = points.splice(0, 1)
@@ -21,12 +25,10 @@ const headers = points.splice(0, 1)
 const [pointExample] = points
 // rememnet both are in strings, review if you need int/long numbers
 const [, , , , latitud, longitud] = pointExample
-// --
-import records from '@/data/records'
 
 export default {
   props: ['total'],
-  mounted() {
+  mounted () {
     console.log('points', points, 'headers', headers)
     console.log(
       'point example latitud',
@@ -37,19 +39,47 @@ export default {
     this.createMap()
   },
   methods: {
-    createMap() {
-      let maptypes = new H.service.Platform({
-        apikey: API_KEY
-      }).createDefaultLayers()
+    createMap () {
+      var testData = {
+        max: 8,
+        data: [{ lat: latitud, lng: longitud }]
+      }
 
-      return new H.Map(
-        document.getElementById('heatmap-maplima'),
-        maptypes.vector.normal.map,
+      var baseLayer = L.tileLayer(
+        'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
         {
-          zoom: 10,
-          center: { lng: 13.4, lat: 52.51 }
+          attribution: '...',
+          maxZoom: 18
         }
       )
+
+      var cfg = {
+        // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+        // if scaleRadius is false it will be the constant radius used in pixels
+        radius: 2,
+        maxOpacity: 0.8,
+        // scales the radius based on map zoom
+        scaleRadius: true,
+        // if set to false the heatmap uses the global maximum for colorization
+        // if activated: uses the data maximum within the current map boundaries
+        //   (there will always be a red spot with useLocalExtremas true)
+        useLocalExtrema: true,
+        // which field name in your data represents the latitude - default "lat"
+        latField: 'lat',
+        // which field name in your data represents the longitude - default "lng"
+        lngField: 'lng',
+        // which field name in your data represents the data value - default "value"
+        valueField: 'count'
+      }
+      //
+      var heatmapLayer = new HeatmapOverlay(cfg)
+
+      var map = new L.Map('heatmap-maplima', {
+        center: new L.LatLng(25.6586, -80.3568),
+        zoom: 4,
+        layers: [baseLayer, heatmapLayer]
+      })
+      heatmapLayer.setData(testData)
     }
   }
 }
@@ -78,6 +108,7 @@ export default {
   left: 0;
   height: calc(100vh - 60px);
   width: 100%;
+  display: block;
 }
 .imgpreload {
   display: -webkit-flex;
